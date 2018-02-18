87b1d8aa9822d31404963c3d002a627a144579fa
Smartec <smartec@mohammed-hossam.local>
2018-02-18 14:00:22 +0200

    Initial Commit

---
+++ moka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moka
+//
+//  Created by Smartec on 2/18/18.
+//  Copyright © 2018 Smartec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
